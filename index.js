e9c9b2e8ff3019d9c10769f8b6089c42fc8739e0
NRenukeshwar <noolurenu.97@gmail.com>
2020-04-17 00:27:33 +0530

    1 file modified, 1 deleted

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
 import './style.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'React'
+      employee:[
+        {id:"2020", name:"Ram", email:"ram@wipro.com"},
+        {id:"2021", name:"Ravi", email:"ravi@wipro.com"},
+        {id:"2022", name:"Vinay", email:"vinay@wipro.com"},
+        {id:"2023", name:"John", email:"john@wipro.com"},
+        {id:"2024", name:"Steve", email:"steve@wipro.com"}
+      ]
     };
   }
 
   render() {
+    const rows=this.state.employee.map(e => <tr>
+          <td>{e.id}</td>
+          <td>{e.name}</td>
+          <td>{e.email}</td>
+        </tr>)
     return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+      <div align="center">
+        <h1>Employee List</h1>
+        <table border="1" style={{textAlign:"center"}}>
+          <thead>
+            <th>Employee ID</th>
+            <th>Employee Name</th>
+            <th>Employee Email ID</th>
+          </thead>
+          <tbody>
+            {rows}
+          </tbody>
+        </table>
       </div>
     );
   }
